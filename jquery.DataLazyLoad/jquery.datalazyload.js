ae1b1fe14d7768bf8168d9d2aedb06d27921c632
liunwcj <120830586@qq.com>
2013-07-24 18:13:20 +0800

    懒加载封�

---
+++ jquery.DataLazyLoad/jquery.datalazyload.js
@@ -0,0 +1,16 @@
+(function($,window){
+    var $window = $(window);
+    $.fn.datalazyload = function(){
+        var $this = $(this);
+        $window.scroll(function(){
+            var topH = $window.height()+$window.scrollTop();
+            $this.each(function(){
+                var $textarea = $(this).find('textarea');
+                if($textarea.css('display') != 'none' && topH >= $(this).offset().top){
+                    $textarea.before($textarea.val()).css('display','none');
+                }
+            });
+        });
+        $window.trigger('scroll');
+    };
+})(jQuery,window);
